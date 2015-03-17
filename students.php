7e386227a95326f1c48786d3107271f0b3a2342b
Emaad Zaamout <emaadeddin.zaamout@uleth.ca>
2015-03-16 18:35:54 -0600

    testing change
    removing aurthors comment on students.php

--- students.php
+++ students.php
@@ -1,10 +1,4 @@
 <?php
-/********************************************************** 
- * Project Name: Driving School System
- * Version: 1.0
- * Developer: Saransh Kalia & Emad Zamout
- * Course: CPSC 3660
- **********************************************************/
 
  session_start();
 if(!isset($_SESSION['user'])){
