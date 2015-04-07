f4b47070dc393e80b228c3108518b56be5204520
skalia11 <KALIAS@Saranshs-MacBook-Pro.local>
2015-04-07 07:59:27 -0600

    New Pages

--- payments.php
+++ payments.php
@@ -1,101 +1,112 @@
-<!--*****************************************************
+<?php
+/********************************************************** 
  * Project Name: Driving School System
  * Version: 1.0
  * Developer: Saransh Kalia & Emad Zamout
  * Course: CPSC 3660
- *****************************************************-->
+ **********************************************************/
 
-<head> 
-	<?php
-		session_start();
-		if(!isset($_SESSION['user']))
-		{
-			header('Location:index.php');
-		}
-		include 'includes/dashboard_header.php' 
-	?>
-		<link rel="stylesheet" href="css/jquery-nicemodal.css">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="js/jquery-nicemodal.js"></script>
-</head>
-<body>
-	<div id="header">
-		<div class="inHeader">
-			<div class="mosAdmin">
-				Hello, <?php echo ucfirst($_SESSION['user']);?><br>
-				| <a href="includes/logout.php">Logout</a>
-			</div>
-			<div class="clear"></div>
-		</div>
-	</div>
+ session_start();
+if(!isset($_SESSION['user'])){
+header('Location:index.php');
+}
+include 'includes/dashboard_header.php' 
+?>
+<link rel="stylesheet" href="css/jquery-nicemodal.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/jquery-nicemodal.js"></script>
 
-	<div id="wrapper">
-		<div id="leftBar">
-			<ul>
-				<?php include'includes/dashboard_menu.php'; ?>
-			</ul>
-		</div>
-		<div id="rightContent">
-			<h3>Students</h3>
-			<hr />
 
-			<div class="shortcutHome"> </div>
+  </head>
+  <body>
+ 
+<div id="header">
+  <div class="inHeader">
+    <div class="mosAdmin">
+    Hello, <?php echo ucfirst($_SESSION['user']);
 
-			<table class="data">
-				<tr class="data">
-					<th class="data">Driving License</th>
-					<th class="data">First Name</th>
-					<th class="data">Last name</th>
-					<th class="data">D.O.B</th>
-					<th class="data">Gender</th>
-					<th class="data">Balance</th>
-					<th class="data"></th>
-				</tr>
-				<tr class="data">
-					<?php
-						include 'includes/connect.php';
-						$sqls= mysqli_query($dbhandle,"SELECT * FROM PEOPLE  join student on student.sincard=people.sincard");
-						$sqlr= mysqli_num_rows($sqls);
+    ?><br>
+   | <a href="includes/logout.php">Logout</a>
+    </div>
+  <div class="clear"></div>
+  </div>
+</div>
 
-						while($sqlf=mysqli_fetch_assoc($sqls))
-						{
-					?>
-					<td class="data" width="30px"><center><?php echo $sqlf['dl_number']; ?></center></td>
-					<td class="data"><center><?php echo ucfirst($sqlf['first_name']); ?></center></td>
-					<td class="data"><center><?php echo ucfirst($sqlf['last_name']); ?></center></td>
-					<td class="data"><center><?php echo $sqlf['date_birth']; ?></center></td>
-					<td class="data"><center><?php echo $sqlf['gender']; ?></center></td>
-					<td class="data"><center><?php echo "$". number_format($sqlf['balance'], 2); ?></center></td>
-					<td class="data">
-						<center>
-							<?php  $sqlres=$sqlf['sincard']; ?>
-							<button> <a href="edit-payments.php?sincard=<?php echo $sqlres;?>"><img src="css/img/edit.png"></a></button>
-						</center>
-					</td>
-				</tr><?php } ?>
-			</table>
+<div id="wrapper">
+  <div id="leftBar">
+  <ul>
+ <?php
+include'includes/dashboard_menu.php'
+ ?>
+  </ul>
+  </div>
+  <div id="rightContent">
+  <h3>Student Payments</h3>
 
-		</div>
-	</div>
-		<script>
-			$(function(){
-				$('button#demo').nicemodal({
-					width: '500px',
-					keyCodeToClose: 27,
-					defaultCloseButton: true,
-					idToClose: '#close-nicemodal',
-					closeOnClickOverlay: true,
-					closeOnDblClickOverlay: false,
-					// onOpenModal: function(){
-					//     alert('Opened');
-					// },
-					// onCloseModal: function(){
-					//     alert('Closed');
-					// }
-				});
-			});
-		</script>
-</body>
+  <hr />
+    
+    <div class="shortcutHome"> </div>
+
+  <table class="data">
+      <tr class="data">
+         <th class="data">Driving License</th>
+        <th class="data">First Name</th>
+        <th class="data">Last name</th>
+        <th class="data">Balance</th>
+
+
+        <th class="data"></th>
+      </tr>
+      <tr class="data">
+
+          <?php
+include 'includes/connect.php';
+$sqls= mysqli_query($dbhandle,"SELECT * FROM PEOPLE  join student on student.sincard=people.sincard");
+
+$sqlr= mysqli_num_rows($sqls);
+while($sqlf=mysqli_fetch_assoc($sqls)){
+   ?>
+        <td class="data" width="30px"><center><?php echo $sqlf['dl_number']; ?></center></td>
+        <td class="data"><center><?php echo ucfirst($sqlf['first_name']); ?></center></td>
+        <td class="data"><center><?php echo ucfirst($sqlf['last_name']); ?></center></td>
+		        <td class="data"><center><?php echo "$". number_format($sqlf['balance'], 2); ?></center></td>
+
+
+        <td class="data">
+        <center>
+         <?php  $sqlres=$sqlf['sincard']; ?>
+        <button> <a href="edit-payments.php?sincard=<?php echo $sqlres;?>">Add Payment</a></button>
+
+        </center>
+        </td>
+      </tr><?php }
+    ?>
+    </table>
+
+</div>
+
+<script>
+$(function(){
+
+    $('button#demo').nicemodal({
+        width: '500px',
+        keyCodeToClose: 27,
+        defaultCloseButton: true,
+        idToClose: '#close-nicemodal',
+        closeOnClickOverlay: true,
+        closeOnDblClickOverlay: false,
+        // onOpenModal: function(){
+        //     alert('Opened');
+        // },
+        // onCloseModal: function(){
+        //     alert('Closed');
+        // }
+    });
+});
+</script>
+
 <?php 
-	include 'includes/dashboard_footer.php'
+
+include 'includes/dashboard_footer.php'
+
 ?>
--- students.php
+++ students.php
@@ -1,94 +1,99 @@
-<!--******************************************************
+<?php
+/********************************************************** 
  * Project Name: Driving School System
  * Version: 1.0
  * Developer: Saransh Kalia & Emad Zamout
  * Course: CPSC 3660
- *******************************************************-->
+ **********************************************************/
 
-<head>
+ session_start();
+if(!isset($_SESSION['user'])){
+header('Location:index.php');
+}
+include 'includes/dashboard_header.php' 
+?>
+<link rel="stylesheet" href="css/jquery-nicemodal.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/jquery-nicemodal.js"></script>
 
-	<?php
-		session_start();
-		if(!isset($_SESSION['user']))
-		{
-			header('Location:index.php');
-		}
-		include 'includes/dashboard_header.php' 
-	?>
 
-	<link rel="stylesheet" href="css/jquery-nicemodal.css">
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="js/jquery-nicemodal.js"></script>
+  </head>
+  <body>
+ 
+<div id="header">
+  <div class="inHeader">
+    <div class="mosAdmin">
+    Hello, <?php echo ucfirst($_SESSION['user']);
 
-</head>
-<body>
+    ?><br>
+   | <a href="includes/logout.php">Logout</a>
+    </div>
+  <div class="clear"></div>
+  </div>
+</div>
 
-	<div id="header">
-		<div class="inHeader">
-			<div class="mosAdmin">
-				Hello, <?php echo ucfirst($_SESSION['user']);?><br>
-				| <a href="includes/logout.php">Logout</a>
-			</div>
-			<div class="clear"></div>
-		</div>
-	</div>
+<div id="wrapper">
+  <div id="leftBar">
+  <ul>
+ <?php
+include'includes/dashboard_menu.php'
+ ?>
+  </ul>
+  </div>
+  <div id="rightContent">
+  <h3>Students</h3>
 
-	<div id="wrapper">
-		<div id="leftBar">
-			<ul>
-				<?php include'includes/dashboard_menu.php';?>
-			</ul>
-		</div>
-		<div id="rightContent">
-			<h3>Students</h3>
-			<hr />
+  <hr />
+     <div id="smallCenter"><a href="create-student.php"><br><h3><div align="center" class="btn">Create a New Student</div></h3></a></div>
+  
+  
+    
+    <div class="shortcutHome">
+    
+  </div>
 
-			<div id="smallCenter">
-				<a href="create-student.php">
-					<br>
-					<h3>
-						<div align="center" class="btn">Create a New Student</div>
-					</h3>
-				</a>
-			</div>
-     
-			<div class="shortcutHome"></div>
+  <table class="data">
+      <tr class="data">
+         <th class="data">Driving License</th>
+        <th class="data">First Name</th>
+        <th class="data">Last name</th>
+        <th class="data">Phone</th>
+        <th class="data">D.O.B</th>
+        <th class="data">Gender</th>
 
-			<table class="data">
-				<tr class="data">
-				<th class="data">Driving License</th>
-				<th class="data">First Name</th>
-				<th class="data">Last name</th>
-				<th class="data">Phone</th>
-				<th class="data">D.O.B</th>
-				<th class="data">Gender</th>
-				<th class="data"></th>
-				</tr>
-				<tr class="data">
 
-				<?php
-					include 'includes/connect.php';
-					$sqls= mysqli_query($dbhandle,"SELECT people.sincard, people.first_name,people.last_name,student.dl_number,people.phone_num,people.date_birth,people.gender FROM PEOPLE  join student on student.sincard=people.sincard");
-					$sqlr= mysqli_num_rows($sqls);
+        <th class="data"></th>
+      </tr>
+      <tr class="data">
 
-					while($sqlf=mysqli_fetch_assoc($sqls))
-					{
-				?>
-						<td class="data" width="30px"><center><?php echo $sqlf['dl_number']; ?></center></td>
-						<td class="data"><center><?php echo ucfirst($sqlf['first_name']); ?></center></td>
-						<td class="data"><center><?php echo ucfirst($sqlf['last_name']); ?></center></td>
-						<td class="data"><center><?php echo $sqlf['phone_num']; ?></center></td>
-						<td class="data"><center><?php echo $sqlf['date_birth']; ?></center></td>
-						<td class="data"><center><?php echo $sqlf['gender']; ?></center></td>
+          <?php
+include 'includes/connect.php';
+$sqls= mysqli_query($dbhandle,"SELECT people.sincard, people.first_name,people.last_name,student.dl_number,people.phone_num,people.date_birth,people.gender FROM PEOPLE  join student on student.sincard=people.sincard");
+$sqlr= mysqli_num_rows($sqls);
+while($sqlf=mysqli_fetch_assoc($sqls)){
+   ?>
+        <td class="data" width="30px"><center><?php echo $sqlf['dl_number']; ?></center></td>
+        <td class="data"><center><?php echo ucfirst($sqlf['first_name']); ?></center></td>
+        <td class="data"><center><?php echo ucfirst($sqlf['last_name']); ?></center></td>
+        <td class="data"><center><?php echo $sqlf['phone_num']; ?></center></td>
+        <td class="data"><center><?php echo $sqlf['date_birth']; ?></center></td>
+        <td class="data"><center><?php echo $sqlf['gender']; ?></center></td>
 
-						<td class="data"><center><?php  $sqlres=$sqlf['sincard']; ?>
-							<button name="views" formmethod="post" data-url="view-student.php?sincard=<?php echo $sqlres;?>" id="demo"><img src="css/img/detail.png"></button>
-							<button> <a href="edit-student.php?sincard=<?php echo $sqlres;?>"><img src="css/img/edit.png"></a></button>
-							<button name="delete" id="demo" formmethod="post" data-url="delete-student.php?sincard=<?php echo $sqlres;?>">  <img src="css/img/delete.png"></button>
-						</center></td>
-				</tr> <?php }?>
-			</table>
-		</div>
+        <td class="data">
+        <center>
+         <?php  $sqlres=$sqlf['sincard']; ?>
+        <button name="views" formmethod="post" data-url="view-student.php?sincard=<?php echo $sqlres;?>" id="demo"><img src="css/img/detail.png"></button>
+        <button> <a href="edit-student.php?sincard=<?php echo $sqlres;?>"><img src="css/img/edit.png"></a></button>
+              <button name="delete" id="demo" formmethod="post" data-url="d.php?sincard=<?php echo $sqlres;?>">  <img src="css/img/delete.png"></button>
+
+        </center>
+        </td>
+      </tr><?php }
+
+    ?>
+    </table>
+
+</div>
 
 <script>
 $(function(){
@@ -100,18 +105,21 @@ $(function(){
         idToClose: '#close-nicemodal',
         closeOnClickOverlay: true,
         closeOnDblClickOverlay: false,
-        // onOpenModal: function(){
-        //     alert('Opened');
-        // },
-        // onCloseModal: function(){
-        //     alert('Closed');
-        // }
+          //onCloseModal: function(){
+       // alert('Reload Page to see the effect !');
+         //   window.location.href='student.php';
+
+         //}
+        
+
     });
 });
 </script>
 
 <?php 
+
 include 'includes/dashboard_footer.php'
+
 ?>
 
 
